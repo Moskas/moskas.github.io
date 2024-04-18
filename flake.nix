25f2d153fddbebfc2fb4a9ecb9ef9d70efcbe1a3
Moskas <minemoskas@gmail.com>
2024-04-18 08:33:59 +0200

    Fixed typos, updated CSS style and nix flake
    Updated style.css

--- flake.nix
+++ flake.nix
@@ -4,11 +4,50 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
   };
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let pkgs = import nixpkgs { inherit system; };
-      in with pkgs; {
-        devShells.default =
-          mkShell { buildInputs = [ html-tidy nodePackages.prettier ]; };
-      });
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        version = "09.04.2024";
+      in
+      with pkgs;
+      {
+        devShells.default = mkShell {
+          buildInputs = [
+            html-tidy
+            nodePackages.prettier
+          ];
+        };
+        packages = {
+          default = stdenv.mkDerivation {
+            name = "moskas.github.io";
+            inherit version;
+            src = ./.;
+            buildPhase = ''
+              mkdir -p $out
+            '';
+            installPhase = ''
+              cp -rv $src/* $out
+            '';
+          };
+          posts = stdenv.mkDerivation {
+            name = "moskas.github.io/posts";
+            inherit version;
+            src = ./POSTS;
+            buildPhase = ''
+              mkdir -p $out
+            '';
+            installPhase = ''
+              cp -rv $src/* $out
+            '';
+          };
+        };
+      }
+    );
 }
